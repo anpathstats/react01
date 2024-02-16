84ba2976ca2b0d048c492e2b5cd99c2ff213ea10
An PATH <aktran@path.org>
2024-02-16 14:37:29 +0700

    video 49

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
+import { Outlet} from "react-router-dom";
 
 const App = () => {
 
   return (
-    <div className="App">
-      <Header/>
+    <div className="app-container">
+      <div className="header-container">
+        <Header/>
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container">
 
+        </div>
+      </div>
+      <div className="app-content">
+        <Outlet/>
+      </div>
     </div>
   );
 }
